6453655d39bc4e9e4f9abee4c9c3faa73bab099a
Andrew Hale <andrew.hale003@gmail.com>
2023-04-26 20:15:38 +1000

    fix broken test

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ class TestCountdown:
     def test_game(self):
         cd = countdown.Countdown()
         cd.set_target(90).set_numbers([25, 50, 100, 5, 3, 1])
-        assert cd.target == 920 and cd.numbers == [25, 50, 100, 5, 3, 1]
+        assert cd.target == 90 and cd.numbers == [25, 50, 100, 5, 3, 1]
 
 
 class TestSolverSpecifiedGame:
